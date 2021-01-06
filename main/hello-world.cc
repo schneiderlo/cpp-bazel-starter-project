06f779540433d3a350db6b703ddb7b4e460601e5
schneiderl <schneider.loic.mp@gmail.com>
2021-01-06 10:05:43 +0100

    Stage 2: Project with 2 targets

--- main/hello-world.cc
+++ main/hello-world.cc
@@ -1,15 +1,8 @@
-#include <ctime>
 #include <string>
 #include <iostream>
 
-std::string get_greet(const std::string& who) {
-  return "Hello " + who;
-}
+#include "hello-greet.h"
 
-void print_localtime() {
-  std::time_t result = std::time(nullptr);
-  std::cout << std::asctime(std::localtime(&result));
-}
 
 int main(int argc, char** argv) {
   std::string who = "world";
