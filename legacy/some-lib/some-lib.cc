81e4d91958e58c210ef56bb8565dc2c3a4b67f6b
schneiderl <schneider.loic.mp@gmail.com>
2021-01-06 11:07:57 +0100

    Stage 4: Adding include path

---
+++ legacy/some-lib/some-lib.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "some-lib.h"
+
+void print_some_lib() {
+  std::cout << "The local time is:" << std::endl;
+}
--- lib/my-lib/hello-time.cc
+++ lib/my-lib/hello-time.cc
@@ -1,7 +1,10 @@
 #include <ctime>
 #include <iostream>
 
+#include "legacy/some-lib/include/some-lib.h"
+
 void print_localtime() {
+  print_some_lib();
   std::time_t result = std::time(nullptr);
   std::cout << std::asctime(std::localtime(&result));
 }
